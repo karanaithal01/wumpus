644da96c1bb07ff9d24481364d672fc0d4e0e538
Karan Aithal <69048972+karanaithal01@users.noreply.github.com>
2021-02-01 10:21:18 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = process.env.PORT || 8080;
-
-connect().use(serveStatic(__dirname)).listen(port);
-
-console.log("Running Wumpus World");
-console.log('Running on http://localhost:' + port);
